1b4cb0f3abbd2e31c093f23f013d3e1b52116d05
wangwei664 <wangwei66418@gmail.com>
2021-09-29 16:28:20 +0800

    feat: 增加通过 GitHub Actions 将应用文件自动部署到 GitHub Pages 的功能

--- deploy.sh
+++ deploy.sh
@@ -9,17 +9,28 @@ yarn run build
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
-# 如果是发布到自定义域名
+# deploy to GitHub
+# 若是发布到自定义域名
 # echo 'www.example.com' > CNAME
+
+# 兼容 本地手动部署 和 GitHub Actions 自动部署
+if [ -z "$GITHUB_TOKEN" ]; then
+  msg='docs: deploy via deploy.sh script'
+  githubUrl=git@github.com:wangwei664/wangwei664.github.io.git
+else
+  msg='docs: deploy via GitHub Actions'
+  githubUrl=https://wangwei664:${GITHUB_TOKEN}@github.com/wangwei664/wangwei664.github.io.git
+  git config --global user.name "wangwei664"
+  git config --global user.email "wangwei66418@gmail.com"
+fi
 
 # 初始化 Git 仓库，并指定初始分支名称为 main
 git init --initial-branch=main
 git add -A
-git commit -m 'docs: deploy'
+git commit -m "$msg"
 
-# 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:wangwei664/wangwei664.github.io.git main
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+# 推送到 GitHub: https://wangwei664.github.io
+git push -f $githubUrl main
 
 cd -
+rm -rf docs/.vuepress/dist
